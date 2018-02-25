95c0ed7297198caf9086890b70a6f7c8e3de40a6
TonyB <tbtonyibrown@outlook.com>
2018-02-25 11:23:51 -0500

    general commit 02252018 1123

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,11 +6,41 @@ $(document).ready(function () {
     var crystal2;
     var crystal3;
     var crystal4;
-    var target = 0;
-    var total = 0;
+    var target;
+    var total;
+    var wins=0;
+    var loses=0;
     var modal = document.getElementById('myModal');
 
     // -----FUNCTIONS-----------------------------------------------------------
+
+    //function to start the game
+    function gamestart() {
+        crystal1 = 0;
+        crystal2 = 0;
+        crystal3 = 0;
+        crystal4 = 0;
+        target = 0;
+        total = 0;
+
+        //create a target random number between 19 and 120
+        target = Math.floor(Math.random() * 102) + 19;
+        // show the target number on the screen
+        $("#number").text(target);
+
+        //create a random number for each of 4 crystals between 1 and 12
+        crystal1 = cryst_val();
+        crystal2 = cryst_val();
+        crystal3 = cryst_val();
+        crystal4 = cryst_val();
+
+        // show 0 for total
+        $("#score").text(total);
+
+        // show instructions
+        $("#message").text("Click the gems to try to match the number above.  You win if you match exactly.  You lose if you go over.");
+    }
+
 
     //function to create random numbers for crystals
     function cryst_val() {
@@ -36,9 +66,11 @@ $(document).ready(function () {
     function endofgame(diff) {
         if (diff === 0) {
             $("#endmsg").text("You matched the number and won the game!!");
+            $("#win").text(++wins);
         }
         else {
             $("#endmsg").text("You went over the number by " + diff + ". You lost.  Better luck next time.");
+            $("#los").text(++loses);
         }
 
         // open the modal to show end of game result
@@ -47,28 +79,22 @@ $(document).ready(function () {
         //close the modal
         $("#close").on("click", function () {
             modal.style.display = "none";
-            location.reload();
+            gamestart();
         });
 
     }
 
     // ----- END OF FUNCTIONS----------------------------------------------------
 
-    //create a target random number between 19 and 120
-    target = Math.floor(Math.random() * 102) + 19;
-    // show the target number on the screen
-    $("#number").text(target);
+    // start the game when the window loads;    
+    $(window).on("load", function () {
+        gamestart();
+        $("#win").text(0);
+        $("#los").text(0);
+    });
 
-    // show 0 for total
-    $("#score").text(total);
 
-    $("#message").text("Click the gems to try to match the number above.  You win if you match exactly.  You lose if you go over.");
 
-    //create a random number for each of 4 crystals between 1 and 12
-    crystal1 = cryst_val();
-    crystal2 = cryst_val();
-    crystal3 = cryst_val();
-    crystal4 = cryst_val();
 
     // insert the value into the crystal label
     $("#c1").on("click", function () {
